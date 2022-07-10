67b181807f485285c8ca5d84eb5a20a2f4c4dab4
UgoTonni <anthonynwachukwu79@gmail.com>
2022-07-10 01:23:54 +0100

    initial commit

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const ExpenseItem = () => {
+  return (
+      <div>
+          <h1 className='text-3xl'>Hello World</h1>
+      </div>
+  )
+}
+
+
+export default ExpenseItem
