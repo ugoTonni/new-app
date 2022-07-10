6dde2c53f76925a27a00712ec05f5e37aa50290d
UgoTonni <anthonynwachukwu79@gmail.com>
2022-07-10 21:14:19 +0100

    new commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import ExpenseItem from "./components/ExpenseItem";
+import Expenses from "./components/Expenses";
 
 function App() {
   const expenses = [
@@ -30,26 +30,8 @@ function App() {
   ];
   return (
     <div className="App">
-      <ExpenseItem
-        title={expenses[0].title}
-        amount={expenses[0].amount}
-        date={expenses[0].date}
-      />
-      <ExpenseItem
-        title={expenses[1].title}
-        amount={expenses[1].amount}
-        date={expenses[1].date}
-      />
-      <ExpenseItem
-        title={expenses[2].title}
-        amount={expenses[2].amount}
-        date={expenses[2].date}
-      />
-      <ExpenseItem
-        title={expenses[3].title}
-        amount={expenses[3].amount}
-        date={expenses[3].date}
-      />
+      <Expenses expenses={expenses} />
+      
     </div>
   );
 }
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,19 +1,11 @@
 import React from "react";
 import "./ExpenseItem.css";
+import ExpenseDate from "./ExpenseDate";
 
 const ExpenseItem = (props) => {
-  const year = props.date.getFullYear();
-  const month = props.date.toLocaleString("en-us", { month: "long" });
-  const day = props.date.toLocaleString("en-us", { day: "2-digit" });
-
   return (
     <div className="expense-item">
-      {/* <h1 className='text-3xl'>Hello World</h1> */}
-      <div>
-        <div> {year}</div>
-        <div>{month}</div>
-        <div>{day}</div>
-      </div>
+      <ExpenseDate date={props.date} />
 
       <div className="expense-item__description">
         <h2> {props.title}</h2>
