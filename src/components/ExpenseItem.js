63382482cc676b07976b832caf54635020a4b2d0
UgoTonni <anthonynwachukwu79@gmail.com>
2022-07-10 03:20:55 +0100

    styling added

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,12 +1,26 @@
-import React from 'react'
+import React from "react";
+import "./ExpenseItem.css";
 
-const ExpenseItem = () => {
+const ExpenseItem = (props) => {
+  const year = props.date.getFullYear();
+  const month = props.date.toLocaleString("en-us", { month: "long" });
+  const day = props.date.toLocaleString("en-us", { day: "2-digit" });
+
   return (
+    <div className="expense-item">
+      {/* <h1 className='text-3xl'>Hello World</h1> */}
       <div>
-          <h1 className='text-3xl'>Hello World</h1>
+        <div> {year}</div>
+        <div>{month}</div>
+        <div>{day}</div>
       </div>
-  )
-}
 
+      <div className="expense-item__description">
+        <h2> {props.title}</h2>
+        <div className="expense-item__price">{props.amount}</div>
+      </div>
+    </div>
+  );
+};
 
-export default ExpenseItem
+export default ExpenseItem;
